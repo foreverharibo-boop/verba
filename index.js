3560fffa433c0c9f084d2c1f0e69b310d851a7aa
foreverharibo-boop <foreverharibo@gmail.com>
2026-09-08 16:27:46 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ import {
 } from './core.js';
 
 const EXTENSION_KEY = 'verba';
-const EXTENSION_VERSION = '0.2.10';
+const EXTENSION_VERSION = '0.2.11';
 const STATE_KEY = 'verba_current_translation';
 const SOURCE_VIEW_KEY = 'verba_source_view';
 const CHARACTER_FIELD_KEY = 'verba';
